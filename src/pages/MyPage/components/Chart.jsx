b4e5be74203f2620d89e2ebc1f4e2d69a23bbc80
조연정 <jojoor201@gmail.com>
2025-09-01 23:03:59 +0900

    마이페이지 userInfo

--- src/pages/MyPage/components/Chart.jsx
+++ src/pages/MyPage/components/Chart.jsx
@@ -1,6 +1,25 @@
 import "./Chart.css";
 
 const Chart = () => {
-  return <div>Chart</div>;
+  return (
+    <div className="Chart">
+      <div>별점으로 읽는 취향 지도</div>
+      <div>차트~~~~~</div>
+      <div>
+        <div>
+          <div>별점 평균</div>
+          <div>3.5</div>
+        </div>
+        <div>
+          <div>별점 개수</div>
+          <div>100</div>
+        </div>
+        <div>
+          <div>많이 준 별점</div>
+          <div>5.0</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Chart;
--- src/pages/MyPage/components/UserInfo.jsx
+++ src/pages/MyPage/components/UserInfo.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import "./UserInfo.css";
 import { UserStateContext } from "../../../App";
 import default_profile_img from "../../../assets/default_profile_image.jpg";
+import { Link } from "react-router-dom";
 
 const UserInfo = () => {
   const { user, setUSer } = useContext(UserStateContext);
@@ -10,16 +11,38 @@ const UserInfo = () => {
 
   return (
     <div className="UserInfo">
-      <div>
-        <div className="user_profile_img_div">
+      <div className="userInfo_profile">
+        <div className="user_profile_img_wrapper">
           <img
             src={userImg || default_profile_img}
             className="user_profile_img"
           />
         </div>
-        <div>이름/이메일</div>
+        <div className="userInfo_profile_info">
+          <div className="userInfo_name">
+            <h4>{user?.displayName || ""}</h4>
+          </div>
+          <div className="userInfo_email">{user?.email || ""}</div>
+        </div>
       </div>
-      <div>평가 코멘트 찜한영화</div>
+      <hr className="userInfo_hr" />
+      <div className="userInfo_reviews_wrapper">
+        <Link className="userInfo_reviews">
+          <div className="userInfo_reviews1">평가</div>
+          <div className="userInfo_reviews2">201</div>
+        </Link>
+        <span className="userInfo_reviews_span">|</span>
+        <Link className="userInfo_reviews">
+          <div className="userInfo_reviews1">코멘트</div>
+          <div className="userInfo_reviews2">101</div>
+        </Link>
+        <span className="userInfo_reviews_span">|</span>
+        <Link className="userInfo_reviews">
+          <div className="userInfo_reviews1">찜한 영화</div>
+          <div className="userInfo_reviews2">23</div>
+        </Link>
+      </div>
+      <hr className="userInfo_hr" />
     </div>
   );
 };
