856e9e87ead93a3f862820477e0c209d1cdae5f5
조연정 <jojoor201@gmail.com>
2025-09-02 19:08:34 +0900

    마이페이지 css & api폴더 변경 전 백업

--- src/pages/MyPage/components/Chart.jsx
+++ src/pages/MyPage/components/Chart.jsx
@@ -1,24 +1,31 @@
+import { Link } from "react-router-dom";
 import "./Chart.css";
 
 const Chart = () => {
   return (
     <div className="Chart">
-      <div>별점으로 읽는 취향 지도</div>
-      <div>차트~~~~~</div>
-      <div>
-        <div>
-          <div>별점 평균</div>
-          <div>3.5</div>
-        </div>
-        <div>
-          <div>별점 개수</div>
-          <div>100</div>
-        </div>
-        <div>
-          <div>많이 준 별점</div>
-          <div>5.0</div>
-        </div>
+      <div className="chart_title">
+        <h4>별점으로 읽는 취향 지도</h4>
       </div>
+      <div className="chart_chart_wrapper">
+        <div>차트~~~~~</div>
+      </div>
+      <hr className="chart_hr" />
+      <div className="chart_content_wrapper">
+        <Link className="chart_content">
+          <div className="chart_content_title">별점 평균</div>
+          <div className="chart_content_content">3.5</div>
+        </Link>
+        <Link className="chart_content">
+          <div className="chart_content_title">별점 개수</div>
+          <div className="chart_content_content">100</div>
+        </Link>
+        <Link className="chart_content">
+          <div className="chart_content_title">많이 준 별점</div>
+          <div className="chart_content_content">5.0</div>
+        </Link>
+      </div>
+      <hr className="chart_hr" />
     </div>
   );
 };
--- src/pages/MyPage/components/LikedComments.jsx
+++ src/pages/MyPage/components/LikedComments.jsx
@@ -1,6 +1,17 @@
 import "./LikedComments.css";
 
 const LikedComments = () => {
-  return <div>LikedComments</div>;
+  return (
+    <div className="LikedComments">
+      <div className="likedComments_title">
+        <h4>좋아요</h4>
+      </div>
+      <hr className="likedComments_hr" />
+      <div className="likedComments_content">
+        좋아한 코멘트 <span>0</span>
+      </div>
+      <hr className="likedComments_hr" />
+    </div>
+  );
 };
 export default LikedComments;
