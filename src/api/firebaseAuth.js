cf3909aa2be78a36e31925c6f2e07398a2f5452d
조연정 <jojoor201@gmail.com>
2025-09-30 23:01:17 +0900

    회원가입,탈퇴 firebase db연동

--- src/api/firebaseAuth.js
+++ src/api/firebaseAuth.js
@@ -14,11 +14,32 @@ import {
   sendPasswordResetEmail, // 비밀번호 재설정 이메일보내기
 } from "firebase/auth";
 import app from "./firebase";
+import {
+  doc,
+  setDoc,
+  getDoc,
+  addDoc,
+  collection,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "./firebase";
 
 const provider = new GoogleAuthProvider();
 
 // Auth 객체
 export const auth = getAuth(app);
+
+// Firestore DB  'users'에 저장
+const firebaseDBSetUser = async (EMAIL, NAME, uid, photoURL) => {
+  await setDoc(doc(db, "user", uid), {
+    displayName: NAME,
+    email: EMAIL,
+    favoriteMovies: [], // 빈 배열로 초기화
+    likedComments: [], // 빈 배열로 초기화
+    photoURL: photoURL,
+    uid: uid,
+  });
+};
 
 // 회원가입
 export const signUp = async (EMAIL, PWD, NAME) => {
@@ -32,6 +53,9 @@ export const signUp = async (EMAIL, PWD, NAME) => {
 
   // 이메일 인증 보내기
   await sendEmailVerification(user);
+
+  // firestore에 user컬렉션에 저장
+  await firebaseDBSetUser(EMAIL, NAME, user.uid, user.photoURL);
 
   return user;
 };
@@ -61,6 +85,11 @@ export const logout = async () => {
 // 회원탈퇴
 export const userDelete = async () => {
   const user = auth.currentUser; // 현재 로그인중인 사용자가져오기
+
+  // Firestore에서 해당 유저 문서 삭제
+  await deleteDoc(doc(db, "user", user.uid));
+
+  // Firebase Auth에서 유저 삭제
   await user.delete();
 };
 
@@ -68,11 +97,21 @@ export const userDelete = async () => {
 export const googleSignUp = async () => {
   try {
     const result = await signInWithPopup(auth, provider);
-    // console.log("구글회원가입", result);
+    console.log("구글회원가입", result);
     console.log("새회원인지알아보기", getAdditionalUserInfo(result));
     const isNewUser = getAdditionalUserInfo(result).isNewUser;
     const googleUserName = getAdditionalUserInfo(result).profile.name;
     const googoleProfilePic = getAdditionalUserInfo(result).profile.picture;
+    const googleUserEmail = getAdditionalUserInfo(result).profile.email;
+    const uid = result.user.uid;
+    if (isNewUser) {
+      await firebaseDBSetUser(
+        googleUserEmail,
+        googleUserName,
+        uid,
+        googoleProfilePic
+      );
+    }
 
     return { result, isNewUser, googleUserName, googoleProfilePic };
   } catch (error) {
@@ -96,7 +135,19 @@ export const kakaoSignUp = async (idToken) => {
   const result = await signInWithCredential(getAuth(), credential);
   const user = result.user;
   const photoURL = user.photoURL; //프로필사진
-  // console.log("result", result, ", user", user);
+  console.log("result", result, ", user", user);
+
+  // firebase db에 user저장하기
+  const userDoc = await getDoc(doc(db, "user", user.uid)); // firebase db에 uid가 있는지 확인하기
+  if (!userDoc.exists()) {
+    // db에 uid가 없다면
+    await firebaseDBSetUser(
+      user.email,
+      user.displayName,
+      user.uid,
+      user.photoURL
+    );
+  }
   return user;
 };
 
